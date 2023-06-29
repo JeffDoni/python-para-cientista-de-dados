9c27073f499d241feb761a7bad486e6d9a598d7e
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2023-06-29 11:21:24 -0300

    Primeiros comandos em Python

---
+++ ciclo-01/primeiros -comandos.py
@@ -0,0 +1,37 @@
+# Exibir Mensagem na Tela
+
+print("Hello World!")
+
+
+print("Meu nome é Jefferson")
+print("Esses são meus primeiros passos em Python")
+
+
+# Operações Matemáticas
+
+print(2 + 2)
+print(2 - 2)
+print(2 * 2)
+print(2 / 2)
+print(2 ** 2)
+print(2 % 2)
+print(10 // 3)
+
+
+# Operações Matemáticas com Variáveis
+
+a = 100 + 10
+b = 100 - 30
+
+print(a)
+print(b - a)
+
+# Condicionais
+
+horario = "manhã"
+
+if horario == "manhã":
+    print("Bom dia!")
+
+else:
+    print("Boa tarde!")
